77955d2efd4bad20832625e99acdf96b13161629
Sudheer <sudheerlikeu@gmail.com>
2021-11-14 23:16:08 +0530

    auto pushing chainges repo

--- input_and_eval_function.py
+++ input_and_eval_function.py
@@ -16,4 +16,8 @@ b=input("enter the 'b' value : ")
 print(f"The Value of 'a' is {a} The type of 'a' is : {type(a)}")
 
 
+# hear converting to integer
+a=int(input("Enter the value of a : ")
+b=int(input("Enter the value of b : ")
 
+print(f"The Value of 'a' is {a} The {type(a)}")
