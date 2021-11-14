dd36f640cdb2d3461da2d8ec63d8c60a34034da6
Sudheer <sudheerlikeu@gmail.com>
2021-11-14 22:42:33 +0530

    auto pushing chainges repo

---
+++ input_and_eval_function.py
@@ -0,0 +1,18 @@
+# printing Simple addition caliculater using input and eval 
+a=4
+b=9
+result=a+b
+print(result)
+# 	or 
+print(f"The Addition of 'a' and 'b' is : {result}")
+
+#	Note: Hear we are fixing the a nd b value  so ther is no chaing result 
+
+# Read the Input from command line every time 
+
+a=input("enter the 'a' value : ")
+b=input("enter the 'b' value : ")
+
+
+
+
---
+++ print_with_varibles_and_strings.py
@@ -0,0 +1,41 @@
+#	print with varibles and strigs
+
+x=10
+y=5.0
+lang_name="python scripting"
+
+
+
+print(x)
+print(y)
+print(lang_name)
+
+#	or 
+
+print(x,y,lang_name)
+
+# or
+
+print("{} {} {}".format(x,y,lang_name))
+
+
+print("{} {} {} {} {}".format(y,x,lang_name,y,x))
+
+
+# print in next line 
+
+print(" {} \n {} \n {}".format(x,y,lang_name))
+
+
+print(f"The 'x' value is : {x} \nThe 'y' value is : {y} \nThe 'Language_Name' is : {lang_name}")
+
+
+my_req_output=f"The x value is :{x} \nThe y value is :{y} \nThe Language_Name is :{lang_name}"
+
+
+print(my_req_output)
+
+
+
+
+
